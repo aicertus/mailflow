c588077d77c3b2a1a441312d6e119118dcf90400
almariscal <almariscal93@gmail.com>
2024-10-08 19:26:20 +0200

    Preparing pull request to main

--- src/retriever/__main__.py
+++ src/retriever/__main__.py
@@ -1,1 +1,6 @@
-import retriever
+from retriever import rag_retrieval
+
+retriever = rag_retrieval()
+retriever.read_db()
+response = retriever.query("Resume las reuniones que hay programados para octubre. Indica día del mes, hora, objetivo de la reunión y quién la propone")
+print(response.response)
